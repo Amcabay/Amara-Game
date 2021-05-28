e2f741eafb816c88f573d7062f46f6281b2d6f1c
Amcabay <amca.raftan@gmail.com>
2021-05-28 20:04:14 +0700

    main menu, stage select, coba level 1
    animasi karakter , main menu ke stage select ,  stage select ke main menu

---
+++ New Unity Project/New Unity Project/Assets/Materials/Stage Selection/tombolback.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tombolback : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    void OnMouseDown()
+    {
+        Application.LoadLevel("main menu");
+    }
+
+    void OnMouseUp()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
