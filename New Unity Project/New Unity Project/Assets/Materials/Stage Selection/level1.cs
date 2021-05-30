5896eaa6bb46649803beca6391d9b275dd94ae6b
Amcabay <amca.raftan@gmail.com>
2021-05-30 14:00:54 +0700

    transisi stage ke level 1

---
+++ New Unity Project/New Unity Project/Assets/Materials/Stage Selection/level1.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class level1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    void OnMouseDown()
+    {
+        Application.LoadLevel("Level 1");
+    }
+
+    void OnMouseUp()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
